d7844a6fa51bc9b419686894df6812d03e73523c
martingerardoc <martingerardoc56@gmail.com>
2026-02-27 12:17:13 -0300

    proyecto creado de lista de tareas

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/**
+ * @description Dentro  de una funcion llamada todos: itera el array facturas e  imprime en consola cada uno de los objetos que se encuentran dentro del array. 
+ */
+import { facturas } from "./utilities";
+
+function todos() {
+    for (let i = 0; i < facturas.length; i++) {
+        console.log(facturas[i]);  
+    }
+}
+
+
+/**
+* @description Dentro  de una funcion llamada pendientes: itera el array facturas e  imprime en consola cada uno de los objetos cuyo estado sea igual a pendiente 
+*/
+
+
+/**
+* @description Dentro  de una funcion llamada pagadas: itera el array facturas e  imprime en consola cada uno de los objetos cuyo estado sea igual a pagadas 
+*/
+
+
+/**
+ * @description En el html encontraras un  ul que contiene tres li.  A cada uno de esos li agregaras un escuchador de evento click y utilizaras las fuciones creadas en los pasos anteriores.
+ *  
+ */
